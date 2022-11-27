d1c01c3f492cce9593d65c764cbd4a8a5d457131
drumwolf <drumwolf@drumwolfs-MacBook-Pro.local>
2022-11-27 12:14:36 -0800

    first draft of Homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
 import React from 'react'
+import { Homepage } from './Homepage'
 
 function App() {
   return (
-    <div>
-      <header>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Homepage />
   );
 }
 
---
+++ src/Homepage/Homepage.tsx
@@ -0,0 +1,9 @@
+import { Box } from '@chakra-ui/react'
+
+export const Homepage = () => {
+  return (
+    <Box borderWidth='1px' borderColor="black" margin="auto" bg="green.200" w={['100%', 480, 768, 960, 1200, 1400]}>
+      This is a box
+    </Box>
+  )
+}
